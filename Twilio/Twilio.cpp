8964e181cf97f22c68528a960eff3fa6c15ad4de
Abhishek <76660005+abhishek213-alb@users.noreply.github.com>
2022-02-06 21:56:32 +0530

    Added Twilio

---
+++ Twilio/Twilio.cpp
@@ -0,0 +1,22 @@
+using System; 
+using System.Collections.Generic; 
+using Twilio; 
+using Twilio.Rest.Api.V2010.Account; 
+using Twilio.Types; 
+ 
+class Example 
+{ 
+    static void Main(string[] args) 
+    { 
+        var accountSid = "ENTER SID"; 
+        var authToken = "[AuthToken]"; 
+        TwilioClient.Init(accountSid, authToken); 
+ 
+        var messageOptions = new CreateMessageOptions( 
+            new PhoneNumber("Your Mobile Number"));   
+        messageOptions.MessagingServiceSid = "Enter Service ID";     
+ 
+        var message = MessageResource.Create(messageOptions); 
+        Console.WriteLine(message.Body); 
+    } 
+}
